52325a8801d5cec9c9892721310459886b6d2af9
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2024-11-16 14:06:59 +0200

    push

--- src/Components/GiftComponent.jsx
+++ src/Components/GiftComponent.jsx
@@ -12,30 +12,42 @@ export const GiftComponent = () => {
   };
   return (
     <section className='gift-component'>
-      <div className='child' style={imageStyle}>
-        <div className='background-div'>
-          <div className='hadith-div'>
-            <p>
-              {isDesktop
-              ? 
-              <>
-              Allah said:“Spend in charity, O son of Adam, and I will spend on you.”
-              </>
-              :
-              <>
-              Allah said:<br/>“Spend in charity, O son of Adam, and I will spend on you.”
-              </>
-              }
-              <span>{text}</span>
-            </p>
+      {isDesktop
+        ?
+        <div className='child' style={imageStyle}>
+          <div className='background-div'>
+            <div className='hadith-div'>
+              <p>
+                Allah said:“Spend in charity, O son of Adam, and I will spend on you.”
+                <span>{text}</span>
+              </p>
+            </div>
+            <p className='generosity'>Your generosity can transform lives. Even a small contribution can make a big difference. Let’s work together to build a brighter future for all. Donate now and be part of this blessed cause.</p>
+            <a href='/support-our-journey' className='gift-btn'>
+              <BsGift/>
+              <p>Gift</p>
+            </a>
           </div>
-          <p className='generosity'>Your generosity can transform lives. Even a small contribution can make a big difference. Let’s work together to build a brighter future for all. Donate now and be part of this blessed cause.</p>
+        </div>
+        :
+        <>
+          <div className='child' style={imageStyle}>
+            <div className='background-div'>
+              <div className='hadith-div'>
+                <p>
+                  Allah said:<br/>“Spend in charity, O son of Adam, and I will spend on you.”
+                  <span>{text}</span>
+                </p>
+              </div>
+              <p className='generosity'>Your generosity can transform lives. Even a small contribution can make a big difference. Let’s work together to build a brighter future for all. Donate now and be part of this blessed cause.</p>
+            </div>
+          </div>
           <a href='/support-our-journey' className='gift-btn'>
             <BsGift/>
             <p>Gift</p>
-          </a>
-        </div>
-      </div>
+          </a>        
+        </>
+      }
     </section>
   )
 }
