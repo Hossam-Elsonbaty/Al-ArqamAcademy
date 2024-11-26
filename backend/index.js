6161f6e5b478d371d42bd3fc2092fa174ce88ccf
Mostafa Elsonbaty <armaggg3@gmail.com>
2024-11-26 21:06:17 +0200

    push

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,22 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import userApplication from './models/user.model.js';
 import cors from 'cors';
+import nodemailer from 'nodemailer';
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', // Or use your preferred email service
+  auth: {
+    user: process.env.EMAIL_USER, // Your email address
+    pass: process.env.EMAIL_PASS, // Your email password or app-specific password
+  },
+});
+
 app.post('/api/users-application', async (req, res) => {
   const application = req.body;
   if(
@@ -22,6 +32,31 @@ app.post('/api/users-application', async (req, res) => {
   const newApplication = new userApplication(application);
   try {
     await newApplication.save();
+    const mailOptions = {
+      from: process.env.EMAIL_USER, // Sender address
+      to: 'mostafasonbaty0@gmail.com', // Recipient
+      subject: 'New Application Submitted',
+      text: `
+        A new application has been submitted:
+        Name: ${application.firstName} ${application.lastName}
+        Email: ${application.email}
+        Phone: ${application.phoneNumber}
+        Gender: ${application.gender}
+        Date of Birth: ${application.dob}
+        Address: ${application.address}
+        City: ${application.city}
+        Zip Code: ${application.zipCode}
+        Selected Program: ${application.selectedProgram}
+        Student: ${application.student}
+        `,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
     res.status(201).json({success: true, data: newApplication})
   } catch (error) {
     console.error("Error in create application: ", error.message);
